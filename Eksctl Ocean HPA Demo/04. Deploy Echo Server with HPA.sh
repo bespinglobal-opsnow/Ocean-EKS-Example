6329a622ea192392ce8fa1474fd2afbda6e8a879
김영석Yeongseok Kim <yeongseok.kim@bespinglobal.com>
2020-06-19 10:46:36 +0900

    changed vegeta user data

--- Eksctl Ocean HPA Demo/04. Deploy Echo Server with HPA.sh
+++ Eksctl Ocean HPA Demo/04. Deploy Echo Server with HPA.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-kubectl apply -f https://raw.githubusercontent.com/bespinglobal-opsnow/Ocean-EKS-Example/master/manifests/echo_server_hpa.yaml
+#kubectl apply -f https://raw.githubusercontent.com/bespinglobal-opsnow/Ocean-EKS-Example/master/manifests/echo_server_hpa.yaml
+#kubectl delete -f https://raw.githubusercontent.com/bespinglobal-opsnow/Ocean-EKS-Example/master/manifests/echo_server_hpa.yaml
+#kubectl apply -f https://raw.githubusercontent.com/kmcgrath/k3s-terraform-modules/master/manifests/echo_server-hpa.yaml
+#kubectl delete -f https://raw.githubusercontent.com/kmcgrath/k3s-terraform-modules/master/manifests/echo_server-hpa.yaml
